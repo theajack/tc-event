3e800c9791227c8167eeef350fb09e14cce450be
theajack <1506282385@qq.com>
2021-02-04 23:28:46 +0800

    feat: 去除asyncTest

--- test/cases/index.js
+++ test/cases/index.js
@@ -61,98 +61,90 @@ module.exports = [
     },
     {
         name: '测试all参数', // 可选
-        plugin: 'asyncPlugin',
-        async test (event) {
+        test (event) {
             event.clear();
             const eventName = 'test-all';
-            return await promise((resolve) => {
-                const result = [];
-                event.emit(eventName);
+            const result = [];
+            event.emit(eventName);
 
-                event.regist(eventName, () => {
-                    result.push(1);
-                });
-                event.regist(eventName, {
-                    all: true,
-                    listener () { result.push(2);}
-                });
-                event.regist(eventName, {
-                    all: false,
-                    listener () {result.push(3);}
-                });
-                resolve(result);
+            event.regist(eventName, () => {
+                result.push(1);
             });
+            event.regist(eventName, {
+                all: true,
+                listener () { result.push(2);}
+            });
+            event.regist(eventName, {
+                all: false,
+                listener () {result.push(3);}
+            });
+            return result;
         },
         expect: [1, 2],
     },
     {
         name: '测试once参数',
-        plugin: 'asyncPlugin',
-        async test (event) {
+        test (event) {
             event.clear();
             const eventName = 'test-all';
-            return await promise((resolve) => {
-                const result = [];
+            const result = [];
 
-                event.regist(eventName, () => {
-                    result.push(1);
-                });
-                event.regist(eventName, {
-                    once: true,
-                    listener () { result.push(2);}
-                });
-                event.regist(eventName, {
-                    once: false,
-                    listener () {result.push(3);}
-                });
-                event.emit(eventName);
-                event.emit(eventName);
-                resolve(result);
+            event.regist(eventName, () => {
+                result.push(1);
             });
+            event.regist(eventName, {
+                once: true,
+                listener () { result.push(2);}
+            });
+            event.regist(eventName, {
+                once: false,
+                listener () {result.push(3);}
+            });
+            event.emit(eventName);
+            event.emit(eventName);
+            return result;
         },
         expect: [1, 2, 3, 1, 3],
     },
     {
         name: '测试index和indexBefore参数',
-        plugin: 'asyncPlugin',
-        async test (event) {
+        test (event) {
             event.clear();
             const eventName = 'test-index';
-            return await promise((resolve) => {
-                const result = [];
-                event.regist(eventName, () => {
-                    result.push(1); // 1
-                });
-                event.regist(eventName, () => {
-                    result.push(2); // 1 2
-                });
-                event.regist(eventName, {
-                    index: 0, // 0 1 2
-                    listener () {result.push(3);}
-                });
-                event.regist(eventName, {
-                    index: 1, // 0 1 *1 2
-                    listener () {result.push(4);}
-                });
-                event.regist(eventName, {
-                    index: 1, // 0 1 *1 **1 2
-                    listener () {result.push(5);}
-                });
-                event.regist(eventName, {
-                    index: 1, // 0 ***1 1 *1 **1 2
-                    indexBefore: true,
-                    listener () {result.push(6);}
-                });
-                event.regist(eventName, {
-                    index: 10, // 0 ***1 1 *1 **1 2 10
-                    listener () {result.push(7);}
-                });
-                event.regist(eventName, () => { // 0 ***1 1 *1 **1 2 3 10
-                    result.push(8);
-                });
-                event.emit(eventName);
-                resolve(result);
+            
+            const result = [];
+            event.regist(eventName, () => {
+                result.push(1); // 1
             });
+            event.regist(eventName, () => {
+                result.push(2); // 1 2
+            });
+            event.regist(eventName, {
+                index: 0, // 0 1 2
+                listener () {result.push(3);}
+            });
+            event.regist(eventName, {
+                index: 1, // 0 1 *1 2
+                listener () {result.push(4);}
+            });
+            event.regist(eventName, {
+                index: 1, // 0 1 *1 **1 2
+                listener () {result.push(5);}
+            });
+            event.regist(eventName, {
+                index: 1, // 0 ***1 1 *1 **1 2
+                indexBefore: true,
+                listener () {result.push(6);}
+            });
+            event.regist(eventName, {
+                index: 10, // 0 ***1 1 *1 **1 2 10
+                listener () {result.push(7);}
+            });
+            event.regist(eventName, () => { // 0 ***1 1 *1 **1 2 3 10
+                result.push(8);
+            });
+            event.emit(eventName);
+            return result;
         },
         expect: [3, 6, 1, 4, 5, 2, 8, 7],
     }
